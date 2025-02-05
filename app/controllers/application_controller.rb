2df91be7dddd28f38ad2dd8367a6c403aa8a90d2
buildizer <youssef.hanna@me.com>
2025-02-06 01:47:30 +0200

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :check_browser
+
+private
+
+def check_browser
+  if browser.ie? || browser.safari?('<=10')
+    render plain: "Your browser is not supported. Please update to a modern browser."
+  end
+end
 end
